047a6512b8c23765022ccad964c4a7e25fd5e112
Ashish Yadav <86865336+Ashu-Kumar-yadav@users.noreply.github.com>
2025-05-29 17:53:40 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -8,5 +8,5 @@ API_HASH = environ.get("API_HASH", "693d541b34acfe8fa0c86278b0c31705")
 BOT_TOKEN = environ.get("BOT_TOKEN", "8115522762:AAGHrL16Ya87K44zxwtVouXbfJwuAy2rYjk")
 OWNER = int(environ.get("OWNER", "79732696525"))
 CREDIT = "𝄟⃝‌🐬Ashish𝄟⃝🐬"
-#WEBHOOK = True  # Don't change this
-#PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
+WEBHOOK = True  # Don't change this
+PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
